b1ffc9e80b610199d726e05bc0637e3e6f178ba2
Julia Maria <julia.leite8@aluno.senai.br>
2024-03-05 14:08:23 -0300

    sections changed to src

---
+++ frontend/src/sections/proliferacao/index.js
@@ -0,0 +1,10 @@
+import "../proliferacao/Proliferacao.css";
+function Proliferacao() {
+  return (
+    <>
+      <p className="titulopage">Proliferação da Dengue</p>
+    </>
+  );
+}
+
+export default Proliferacao;
