70ea6e8966b3afdfbae324bd3648b763942c45d2
Henrique Barros Pereira <henrique.pereira@aluno.senai.br>
2024-03-07 08:55:42 -0300

    atualiza

--- frontend/src/sections/SaudePublica/index.js
+++ frontend/src/sections/SaudePublica/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../SaudePublica/Saude.css'
 import { Col, Container, Row } from 'react-bootstrap'
-import mosquito5 from '../img/mosquito5'
+import mosquito5 from '../../img/mosquitocinco.jpg'
 function Saude () {
     return(
         <div className='body1'>
@@ -12,15 +12,15 @@ function Saude () {
 </h1>
 <Container>
     <Row>
-        <Col>
+        <Col sm={8}>
         <div className='texto-1'>
         O aumento dos casos de dengue devido ao acúmulo de sucata e entulho representa um grave risco à saúde pública devido à sobrecarga que impõe ao sistema de saúde. Quando há um grande número de casos de dengue em uma região, os serviços de saúde podem ficar sobrecarregados, levando a dificuldades no atendimento adequado aos pacientes. Além disso, a dengue pode evoluir para formas mais graves da doença, como a dengue hemorrágica, que pode ser fatal se não for tratada de forma adequada e oportuna. 
         </div>
         </Col>
-        <Col>
+        <Col sm={4}>
         <div className='bolinha-1'>
 <div className='foto-1'>
-<img src={mosquito5}
+<img src={mosquito5}/>
 </div>
         </div>
         </Col>
