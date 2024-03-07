7112c00fea2772986ca3b052f2a1209ce2f4c7df
Henrique Barros Pereira <henrique.pereira@aluno.senai.br>
2024-03-07 15:06:02 -0300

    baro da tropa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Proliferacao from "./sections/Proliferacao";
 import Dengue from "./sections/Dengue";
 import Saude from "./sections/SaudePublica/index";
+import NavBarComp from "./components/NavBar/index"
 function App() {
   return (
     <>
