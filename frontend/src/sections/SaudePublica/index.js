70da99a11258aa7573ba065ded4e19095f34e59e
Henrique Barros Pereira <henrique.pereira@aluno.senai.br>
2024-03-07 10:05:57 -0300

    baro finalizaçao

--- frontend/src/sections/SaudePublica/index.js
+++ frontend/src/sections/SaudePublica/index.js
@@ -2,41 +2,41 @@ import React from 'react'
 import '../SaudePublica/Saude.css'
 import { Col, Container, Row } from 'react-bootstrap'
 import mosquito5 from '../../img/mosquitocinco.jpg'
+import mosquito3 from '../../img/mosquitotres.jpg'
 function Saude () {
     return(
         <div className='body1'>
-            <br/>
-            <br/>
+           
 <h1 className='titulo-saude'>
-    baro
+    Riscos a Saúde Publica
 </h1>
 <Container>
     <Row>
-        <Col sm={8}>
-        <div className='texto-1'>
-        O aumento dos casos de dengue devido ao acúmulo de sucata e entulho representa um grave risco à saúde pública devido à sobrecarga que impõe ao sistema de saúde. Quando há um grande número de casos de dengue em uma região, os serviços de saúde podem ficar sobrecarregados, levando a dificuldades no atendimento adequado aos pacientes. Além disso, a dengue pode evoluir para formas mais graves da doença, como a dengue hemorrágica, que pode ser fatal se não for tratada de forma adequada e oportuna. 
+        <Col sm={7}>
+        <div className='texto-1'><p>
+        O aumento dos casos de dengue devido ao acúmulo de sucata e entulho representa um grave risco à saúde pública devido à sobrecarga que impõe ao sistema de saúde. Quando há um grande número de casos de dengue em uma região, os serviços de saúde podem ficar sobrecarregados, levando a dificuldades no atendimento adequado aos pacientes. Além disso, a dengue pode evoluir para formas mais graves da doença, como a dengue hemorrágica, que pode ser fatal se não for tratada de forma adequada e oportuna. </p>
         </div>
         </Col>
-        <Col sm={4}>
+        <Col sm={5}>
         <div className='bolinha-1'>
-<div className='foto-1'>
-<img src={mosquito5}/>
-</div>
+
+<img className='foto-1' src={mosquito5}/>
+
         </div>
         </Col>
     </Row>
     <Row>
-        <Col>
-        <div>
-
+       
+        <Col sm={5}>
+        <div className='bolinha-2'>
+          <img className='foto-1'src={mosquito3}/>
         </div>
         </Col>
-        <Col>
-        <div>
-
+    <Col sm={7}>
+        <div className='texto-2'>
+      <p>A incidência aumentada de casos de dengue também pode impactar a economia local, uma vez que pessoas doentes podem ficar impossibilitadas de trabalhar, gerando perda de produtividade e aumento nos gastos com saúde.Portanto, é crucial combater o acúmulo de sucata e entulho como uma medida preventiva para reduzir os casos de dengue e seus impactos na saúde pública e na sociedade como um todo. Isso pode ser feito através de campanhas de conscientização, fiscalização e incentivo a práticas de descarte adequado de resíduos.</p>  
         </div>
-        </Col>
-    </Row>
+        </Col> </Row>
 </Container>
         </div>
     )
